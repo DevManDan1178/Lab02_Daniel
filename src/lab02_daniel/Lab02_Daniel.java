7471106803a704f4ef47783ae94867352d0ad97e
6323612 <6323612@host.docker.internal>
2025-10-27 09:47:50 -0400

    + added transitions, buttons, and button function functionality

--- src/lab02_daniel/Lab02_Daniel.java
+++ src/lab02_daniel/Lab02_Daniel.java
@@ -5,6 +5,8 @@
 package lab02_daniel;
 
 
+import java.util.HashSet;
+import java.util.Set;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -12,10 +14,14 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
-import java.util.Random;
+import javafx.animation.FadeTransition;
+import javafx.animation.Timeline;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
-import javafx.scene.text.TextAlignment;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 /**
  *
@@ -27,44 +33,95 @@ public class Lab02_Daniel extends Application{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         launch(args);
     }
         
 
     @Override
     public void start(Stage primaryStage) {
+        final double IMAGE_TRANSITION_DURATION = 2;
+        Image[] IMAGES = new Image[20];
+        
+        for (int i = 0; i < 20; i++) {     
+            int imgPathIndex = 101 + i;
+            IMAGES[i] = new Image("file:images/" + imgPathIndex + ".jpg");
+        }
+        int[] index = {0}; //must be array to be modifiable in lambda
+        
         // Set size and title
         primaryStage.setWidth(250);
         primaryStage.setHeight(300);
         primaryStage.setTitle("Java Games");
         
         // Create root and scene
-        BorderPane rootBorderPane = new BorderPane();
-        Scene scene = new Scene(rootBorderPane);
+        VBox rootVBox = new VBox();
+        BorderPane borderPane = new BorderPane();
+        
+        Scene scene = new Scene(rootVBox);
+        
         // Create other elements
         VBox middle = new VBox();
         Label topLabel = new Label("Random Game");
         Label botLabel = new Label("Waiting....");
         Label lblImage = new Label();
-        
+        ImageView imgView = new ImageView(IMAGES[0]);
+        lblImage.setGraphic(imgView);
         // Setup structure elements
-        rootBorderPane.setTop(topLabel);
-        rootBorderPane.setBottom(botLabel);
-        rootBorderPane.setCenter(middle);
-        middle.getChildren().add(lblImage);
+        borderPane.setTop(topLabel);
+        borderPane.setBottom(botLabel);
+        borderPane.setCenter(middle);
+        middle.getChildren().addAll(lblImage, botLabel);
         
         // Align structure elements
         middle.setAlignment(Pos.CENTER);
         BorderPane.setAlignment(topLabel, Pos.CENTER);
         BorderPane.setAlignment(botLabel, Pos.CENTER);
-
-        // Set random image
-        Random rng = new Random();
-        int randomImageNumber =  rng.nextInt(101, 121);
-        Image img = new Image("file:images/" + randomImageNumber + ".jpg");
-        lblImage.setGraphic(new ImageView(img));
         
+        //LAB07_B 
+        
+        //Create Button
+        HBox buttonsHBox = new HBox();
+        Button interactButton = new Button("PLAY");
+        Button addSpeedButton = new Button("SPEED +10%");
+        Button subSpeedButton = new Button("SPEED -10%");
+        
+        buttonsHBox.getChildren().addAll(interactButton, addSpeedButton, subSpeedButton);
+        buttonsHBox.setAlignment(Pos.CENTER);
+        buttonsHBox.setSpacing(20);
+        //Transition
+        FadeTransition transition = new FadeTransition();
+        transition.setDuration(Duration.seconds(IMAGE_TRANSITION_DURATION));
+        
+        
+        transition.setOnFinished(e -> {
+            index[0] = (index[0] + 1) % 20;
+            imgView.setImage(IMAGES[index[0]]);
+            transition.playFromStart();
+        });
+        //Button functionality
+        interactButton.setOnAction(e -> {
+            if (interactButton.getText().equals("PLAY")) {
+                transition.play();
+                interactButton.setText("PAUSE");
+            } else {
+                transition.pause();
+                interactButton.setText("PLAY");
+            }
+        });
+        
+        addSpeedButton.setOnAction(e -> {
+            transition.setRate(transition.getRate() * 1.1);
+        });
+        
+        subSpeedButton.setOnAction(e -> {
+           transition.setRate(transition.getRate() * .9);
+        });
+        
+        transition.setNode(borderPane);
+        
+        
+        rootVBox.getChildren().addAll(borderPane, buttonsHBox);
         // Set and display scene/stage
         primaryStage.setScene(scene);
         primaryStage.show();
